52a3d2c4214fbd57390fb7974d5a972ef5b3c112
Kiisu_Master <142301391+Kiisu-Master@users.noreply.github.com>
2024-04-23 23:11:30 +0300

    Fix test.gd formatting

--- test.gd
+++ test.gd
@@ -1,29 +1,41 @@
 @tool
-extends Node
-class_name TestNode
+class_name TestNode extends Node
 
 signal test_signaled
+
 enum TestEnum {
 	CASE_ONE,
 	CASE_TWO,
 }
-const TEST_CONSTANT := Color()  # Constructor.
+
+const TEST_CONSTANT := Color()  # Constructor / Inline comment.
+
 @export var test_export_var := 0
-static var test_static_var: Vector2i
+
 var test_var_node := $Child/Child
-@onready test_onready_var
+
+@onready var test_onready_var
+
+static var test_static_var: Vector2i
 
 
 func _ready() -> void:
-	test_function('Hello world!')
-	TestNode.test_function("This is a test string.\n \" \' This will be in the string.")
+	test_function("Hello world!")
+
+	var number := TestNode.test_function(
+		"This is a test string.\n \" \' This will be in the string."
+	)
+
 	var format_string := "%i" % 10
+	TestClass.test_class_member = format_string
+
 	var multiline_string := """
 This is a multiline string.
 It can be multiple lines long.
 """
-	TestClass.test_class_member += 1.0
-	%UniqueNameNode
+	%UniqueNameNode.text = multiline_string
 
-func test_function(msg: String) -> void:
+
+func test_function(msg: String) -> int:
 	print(msg)
+	return randi()
