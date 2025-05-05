4018a4073aa45d2a20f39884b4041a4aedc09a3a
Rito12 <111280526+Rito13@users.noreply.github.com>
2025-05-05 19:54:45 +0200

    Update test.gd
    Fixed string formating test. 
    Added new tests.

--- test.gd
+++ test.gd
@@ -7,9 +7,9 @@ class_name TestNode extends Node
 signal test_signaled
 
 enum TestEnum {
-	CASE_ONE,
-	CASE_TWO,
-	CASE_THREE,
+        CASE_ONE,
+        CASE_TWO,
+        CASE_THREE,
 }
 
 const TEST_CONSTANT := Color()  # Constructor / Inline comment.
@@ -22,24 +22,33 @@ var test_var_node := $Child/Child  ## Inline docstring.
 
 static var test_static_var: Vector2i
 
+# var commented_string_test = "I like \n carrots"
+# var commented_multiline_string_test = '''
+# First line of text
+# BBCode [color=green]test[/color]
+# '''
+
+@export_range(12,23) var test_float = 18.13_200_001
+@export_flags("A","B","C","D") var bits = 0b1001
+@export var purple_float = 0xAA00ff
 
 func _ready() -> void:
-	test_function("Hello world!")
+        test_function("Hello world!")
 
-	var number := TestNode.test_function(
-		"This is a test string.\n \" \' This will be highlighted as part of the string."
-	)
+        var number := TestNode.test_function(
+                "This is a test string.\n \" \' This will be highlighted as part of the string."
+        )
 
-	match number:
-		TestEnum.CASE_ONE when true:
-			continue
-		_:
-			return
+        match number:
+                TestEnum.CASE_ONE when true:
+                        continue
+                _:
+                        return
 
-	var format_string := "%i" % 10
-	TestClass.test_class_member = format_string
+        var format_string := "time: %dh" % 10
+        TestClass.test_class_member = format_string
 
-	var multiline_string := """
+        var multiline_string := """
 
 This is a multiline string.
 It can be multiple lines long.
@@ -48,9 +57,22 @@ These should be highlighted: \n \n \\\\
 This shouldn't be highlighted: \
 
 """
-	%UniqueNameNode.text = multiline_string
+        var multiline_string_2 = r'''
+This is raw string
+because it is prefixed with 'r'
+highlighted: \r \n \a \f
+not highlighted: \ \d \g
+'''
+        %UniqueNameNode.text = multiline_string
 
+        var path := ^"../sibling/child"
+# line ending with space 
+# and with tab 		
+
+func new_function(haha:=1.23) -> StringName:
+    var string_name := &"Named_Node"
+    return string_name
 
 func test_function(msg: String) -> int:
-	print(msg)
-	return randi() % 2
+        print(msg)
+        return randi() % 2
